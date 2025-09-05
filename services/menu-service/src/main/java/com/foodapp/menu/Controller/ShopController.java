f233a2b2fa5d29a1c6a8fd7f0485f9053381d12c
artempuskarev153@gmail.com <170014564+KawendiW@users.noreply.github.com>
2025-09-05 21:16:40 +0500

    feat(menu-service): add endpoints get all shop and get by slug (GET /api/v1/shops/{slug} or without {slug)

--- services/menu-service/src/main/java/com/foodapp/menu/Controller/ShopController.java
+++ services/menu-service/src/main/java/com/foodapp/menu/Controller/ShopController.java
@@ -1,31 +1,81 @@
 package com.foodapp.menu.Controller;
 
+import com.foodapp.menu.Controller.DTO.PageResponse;
 import com.foodapp.menu.Controller.DTO.ShopCreateRequest;
 import com.foodapp.menu.Controller.DTO.ShopSummaryResponse;
+import com.foodapp.menu.Controller.DTO.ShopUpdateRequest;
 import com.foodapp.menu.Service.ShopService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("${api.base-path}/shops")
+@RequestMapping(
+        value = "${api.base-path}/shops",
+        produces = MediaType.APPLICATION_JSON_VALUE)
 public class ShopController {
 
     private final ShopService shopService;
 
-    @PostMapping("/create")
-    public ResponseEntity<ShopSummaryResponse> createShop(@Valid @RequestBody ShopCreateRequest request){
+    @GetMapping()
+    public ResponseEntity<PageResponse<ShopSummaryResponse>> getAllShop(@PageableDefault(size = 20) Pageable pageable) {
+        PageResponse<ShopSummaryResponse> shop = shopService.getAllPageable(pageable);
+
+        return ResponseEntity
+                .ok(shop);
+    }
+
+
+    @GetMapping("/{slug}")
+    public ResponseEntity<ShopSummaryResponse> getShopBySlug(@PathVariable String slug){
+        ShopSummaryResponse shop = shopService.getBySlug(slug);
+
+        return ResponseEntity
+                .ok(shop);
+    }
+
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ShopSummaryResponse> createShop(@Valid @RequestBody ShopCreateRequest request) {
         ShopSummaryResponse shop = shopService.createShop(request);
-        URI location = URI.create("/api/v1/shops/%s".formatted(shop.getSlug()));
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{slug}")
+                .buildAndExpand(shop.getSlug())
+                .toUri();
 
         return ResponseEntity
                 .created(location)
                 .body(shop);
+    }
+
+    @PatchMapping(path = "/{slug}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ShopSummaryResponse> updateShop(@Valid @RequestBody ShopUpdateRequest request,
+                                                          @PathVariable String slug) {
+        ShopSummaryResponse shop = shopService.updateShop(slug, request);
+
+        return ResponseEntity
+                .ok(shop);
+    }
+
+    @DeleteMapping("/{slug}")
+    public ResponseEntity<Void> deleteShop(@PathVariable String slug) {
+        shopService.deleteShop(slug);
+
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+
     }
 
 }
