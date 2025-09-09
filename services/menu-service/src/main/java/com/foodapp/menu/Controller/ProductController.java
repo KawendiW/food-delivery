0675da565d2a1f176eb8def911b6db71b8c06cc9
artempuskarev153@gmail.com <170014564+KawendiW@users.noreply.github.com>
2025-09-09 17:50:46 +0500

    feat(menu-service): add endpoint create product (POST /api/v1/shops/shopSlug/products)

--- services/menu-service/src/main/java/com/foodapp/menu/Controller/ProductController.java
+++ services/menu-service/src/main/java/com/foodapp/menu/Controller/ProductController.java
@@ -1,27 +1,29 @@
 package com.foodapp.menu.Controller;
 
 import com.foodapp.menu.Controller.DTO.PageResponse;
+import com.foodapp.menu.Controller.DTO.ProductCreateRequest;
 import com.foodapp.menu.Controller.DTO.ProductSummaryResponse;
 import com.foodapp.menu.Service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "${api.base-path}/products",
+@RequestMapping(value = "${api.base-path}/shops/{shopSlug}/products",
         produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping("/{shopSlug}")
+    @GetMapping()
     public ResponseEntity<PageResponse<ProductSummaryResponse>> getAllByPage(@PageableDefault(size = 20) Pageable pageable,
                                                                              @PathVariable String shopSlug) {
         PageResponse<ProductSummaryResponse> product = productService.getAllPageable(shopSlug, pageable);
@@ -30,12 +32,30 @@ public class ProductController {
                 .ok(product);
     }
 
-    @GetMapping("/{shopSlug}/{sku}")
+    @GetMapping("/{sku}")
     public ResponseEntity<ProductSummaryResponse> getProductBySku(@PathVariable String shopSlug,
-                                                                  @PathVariable String sku){
+                                                                  @PathVariable String sku) {
         ProductSummaryResponse product = productService.getBySku(shopSlug, sku);
 
         return ResponseEntity
                 .ok(product);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ProductSummaryResponse> createProduct(@PathVariable String shopSlug,
+                                                                @Valid @RequestBody ProductCreateRequest request) {
+
+        ProductSummaryResponse product = productService.createProduct(shopSlug, request);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{sku}")
+                .buildAndExpand(product.getSku())
+                .toUri();
+
+
+        return ResponseEntity
+                .created(location)
+                .body(product);
     }
 }
